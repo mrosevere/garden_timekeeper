0186656e253b37bdc776a2eff50960d718e99d97
mrosevere <mark.rosevere@gmail.com>
2026-02-07 16:41:15 +0000

    create beds model and html

--- core/models.py
+++ core/models.py
@@ -7,5 +7,24 @@ in other apps.
 """
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class GardenBed(models.Model):
+    """A physical garden bed or container owned by a user."""
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="garden_beds"
+    )
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
