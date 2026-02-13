ad2233a5146a700999fcf237ab259033928568a3
mrosevere <mark.rosevere@gmail.com>
2026-02-13 16:15:55 +0000

    fix issue-116 - username is case sensitive

---
+++ core/auth_backends.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+class CaseInsensitiveUsernameBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None:
+            return None
+
+        try:
+            user = UserModel.objects.get(username__iexact=username)
+        except UserModel.DoesNotExist:
+            return None
+
+        if user.check_password(password):
+            return user
+
+        return None
